13435a9f702033f1ebdbc479f4b3f2965f4970d9
dongjin <gksgks666@naver.com>
2025-03-24 13:23:35 +0900

    feat: button 컴포넌트 추가

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import { ComponentProps, ReactNode } from "react";
+
+interface Props extends ComponentProps<"button"> {
+  variant?: "primary" | "secondary";
+  size?: "small" | "medium" | "large";
+  className?: string;
+  onClick?: () => void;
+  children: ReactNode;
+}
+
+export default function Button({
+  className,
+  onClick,
+  children,
+  ...props
+}: Props) {
+  return (
+    <button className={className} onClick={onClick} {...props}>
+      {children}
+    </button>
+  );
+}
