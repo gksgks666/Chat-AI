e98a3711a52acd06d4b2fcee76db8ba7c3f1ade7
dongjin <gksgks666@naver.com>
2025-03-24 13:26:24 +0900

    feat: 채팅창, 전송버튼 기능 구현

--- src/components/ChatInput.tsx/index.tsx
+++ src/components/ChatInput.tsx/index.tsx
@@ -1,24 +1,40 @@
 "use client";
 
 import { useState } from "react";
+import Button from "@/components/Button";
 
 export default function ChatInput() {
   const [message, setMessage] = useState("");
 
+  const handleSend = () => {
+    if (message.trim() !== "") {
+      console.log("Sending message:", message);
+      setMessage("");
+    }
+  };
+
   return (
     <div className="flex flex-col items-start p-4 bg-white">
-      <div className="border-1 border-gray-300 border-opacity-50 rounded-lg p-2 shadow-lg w-[400px] sm:w-[600px] md:w-[800px] lg:w-[1000px]">
+      <div className="border-1 border-gray-300 border-opacity-50 rounded-lg p-2 shadow-lg w-[450px] sm:w-[600px] md:w-[750px] lg:w-[1000px]">
         <textarea
           className="block h-20 w-full resize-none border-0 focus:ring-0 focus:border-transparent outline-none"
           placeholder="대화 내용을 입력해 주세요."
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) =>
+            e.key === "Enter" &&
+            !e.shiftKey &&
+            (e.preventDefault(), handleSend())
+          }
         />
       </div>
 
-      <button className="px-4 py-2 mt-2 text-white bg-[#B8E986] hover:bg-[#A3D375] rounded-lg cursor-pointer">
+      <Button
+        className="px-4 py-2 mt-2 text-white bg-[#B8E986] hover:bg-[#A3D375] rounded-lg cursor-pointer"
+        onClick={handleSend}
+      >
         전송
-      </button>
+      </Button>
     </div>
   );
 }
