3f72b0f942831dc498e6f2a4675929ea4976ee04
dongjin <gksgks666@naver.com>
2025-03-25 20:50:45 +0900

    feat: 대화창 진입 첫 화면 구성

---
+++ src/components/ChatContent/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ChatContent() {
+  return (
+    <div className="flex items-center justify-center text-3xl m-4 h-full w-[450px] sm:w-[600px] md:w-[750px] lg:w-[1000px] min-h-[300px]">
+      무엇을 도와드릴까요?
+    </div>
+  );
+}
